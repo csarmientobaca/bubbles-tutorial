4c5525ed0cb9429e966c21283292c55bd39c317e
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2026-05-26 02:17:22 +0200

    Initial Godot project

---
+++ player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var timeLeft = 60
+
+var SPEED = 300.0
+var JUMP_VELOCITY = -400.0
+
+var myGravity = 5
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += myGravity
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
